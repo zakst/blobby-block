f91c53beed73581e029a71bce7524838bcea2ee9
Zaki Badawi <zaki.badawi@kmart.com.au>
2022-04-27 21:14:59 +1000

    fix: improved eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'deprecation', 'unused-imports'],
   extends: [
+    'airbnb-typescript-prettier',
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -15,11 +16,54 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
   rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-shadow': 'off',
+    'no-use-before-define': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    /** allows any types to be defined */
+    '@typescript-eslint/no-explicit-any': 'on',
+    'import/prefer-default-export': 'off',
+    'no-useless-constructor': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-unresolved': 'off',
+    'import/order': 'error',
+    'max-classes-per-file': ['error', 5],
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { 'exceptAfterSingleLine': true },
+    ],
+    'curly': ['error', 'all'],
+    'deprecation/deprecation': 'warn',
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        "vars": "all",
+        "varsIgnorePattern": "^_",
+        "args": "after-used",
+        "argsIgnorePattern": "^_"
+      }
+    ]
   },
-};
+  'overrides': [
+    {
+      'files': ['**/**/*.spec.ts'],
+      'rules': {
+        '@typescript-eslint/no-explicit-any': 'off'
+      }
+    }
+  ]
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  overrides: [{ files: "*.yml", options: { singleQuote: true } }],
+  singleQuote: true,
+  trailingComma: "all",
+  semi: false,
+  printWidth: 100,
+  htmlWhitespaceSensitivity: 'ignore',
+  arrowParens: "avoid",
+}
