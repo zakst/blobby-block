d4dc5d3997bb82c48053d22ad7b31d0f3d3c6d40
Zaki Badawi <zaki.badawi@kmart.com.au>
2022-12-31 14:07:36 +1100

    feat: added configurable port

--- apps/blockchain/src/main.ts
+++ apps/blockchain/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { BlockchainModule } from './blockchain.module'
+import * as process from 'process'
+const port = process.env.PORT
 
 async function bootstrap() {
   const app = await NestFactory.create(BlockchainModule)
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000)
+  await app.listen(port)
 }
 bootstrap()
