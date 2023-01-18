5bc5a3dc1f306131d9fc4ef2800a45cf9543bec3
Zaki Badawi <zaki.badawi@kmart.com.au>
2023-01-18 21:07:34 +1100

    feat: added material ui and basic app structure

--- apps/blobby-ui/src/App.tsx
+++ apps/blobby-ui/src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import {
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  TextField
+} from '@material-ui/core'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactbla
-        </a>
-      </header>
-    </div>
-  );
+    <Container maxWidth="xl">
+      <Grid container md={12}
+            direction="row"
+            justifyContent="space-evenly">
+        <Grid md={8}
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center">
+          <FormControl margin={"normal"}>
+            <FormLabel id="search-by">Search By</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="search-by"
+              defaultValue="transaction_id"
+              name="search-by-group"
+            >
+              <FormControlLabel value="transaction_id" control={<Radio/>} label="Transaction Id"/>
+              <FormControlLabel value="block_hash" control={<Radio/>} label="Block Hash"/>
+              <FormControlLabel value="address" control={<Radio/>} label="Address"/>
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid md={8}
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center">
+          <TextField fullWidth id="search-term" label={"Search Term"} variant="outlined" />
+        </Grid>
+      </Grid>
+
+    </Container>
+  )
 }
 
 export default App;
