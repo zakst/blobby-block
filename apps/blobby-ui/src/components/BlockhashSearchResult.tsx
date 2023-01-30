96a1d8b2a474de7591d00929e0c089963f919bf4
zaki <mog5808@gmail.com>
2023-01-30 15:08:08 +1100

    docs: updated the img

---
+++ apps/blobby-ui/src/components/BlockhashSearchResult.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@material-ui/core'
+import TransactionDto from '../dtos/transaction.dto'
+
+export type BlockhashSearchResult = {
+  blockId: number
+  transactions: TransactionDto[]
+}
+
+const BlockhashSearchResult: React.FC<BlockhashSearchResult> = props => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography color="primary" variant="overline">
+          Transaction Id Result
+        </Typography>
+        <Grid direction="row" container md={12} justifyContent="space-between">
+          <Typography color="textSecondary">
+            BlockId
+          </Typography>
+          <Typography color="secondary">
+            {props.blockId}
+          </Typography>
+        </Grid>
+        <Grid direction="row" container md={12} justifyContent="space-between">
+          <TableContainer component={Paper}>
+            <Table aria-label="Transactions Table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Sender</TableCell>
+                  <TableCell>Receiver</TableCell>
+                  <TableCell align="right">Amount</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {props.transactions.map((transaction) => (
+                  <TableRow
+                    key={transaction.transactionId}
+                  >
+                    <TableCell component="th" scope="row">
+                      {transaction.sender}
+                    </TableCell>
+                    <TableCell>{transaction.receiver}</TableCell>
+                    <TableCell align="right">{transaction.amount}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+
+      </CardContent>
+    </Card>
+  )
+}
+
+export default BlockhashSearchResult
