77fdd5ea644a640a1544bc3a3e74e2b191bd2559
zaki <mog5808@gmail.com>
2023-02-04 19:09:21 +1100

    fix: readability

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 clear
+
 NODE_BASE_URL='http://localhost:'
+
 # Update the port numbers with the ones you chose to run the blobby block on
 NODE_URLS=($NODE_BASE_URL'5000' $NODE_BASE_URL'5001' $NODE_BASE_URL'5002' $NODE_BASE_URL'5003')
 
